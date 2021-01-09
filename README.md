f0d260b93b0ca338179e0acd2b4a11e1b193e09c
tge96 <tge96@users.noreply.github.com>
2021-01-09 14:07:35 -0800

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ee101-MacOS-py
-Python port of www.ee101.com firmware debugger APIs to MacOS
+# ee101-macOS-py
+Python port of www.ee101.com firmware debugger APIs to macOS 
+
+v0.1 - asks user to enter index from a list of available serial ports
--- test_ee101_1wire.py
+++ test_ee101_1wire.py
@@ -1,5 +1,7 @@
+import sys
 import time
 import serial
+from serial.tools.list_ports import comports
 
 def EE101Text(channel, text):
     EE101_SYNC = 0x50
@@ -17,29 +19,62 @@ def EE101Value(channel, value):
     ser.write(bytes([(int(value >> 8))]))
     ser.write(bytes([(int(value) & 0xFF)]))
 
-print("Press CTL+C to exit program")
+def ask_for_port():
+    """\
+    Show a list of ports and ask the user for an index choice.
+    """
+    sys.stderr.write('\nAvailable ports: <index:> <name> <desc> <hwid>\n')
+    ports = []
+    for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
+        sys.stderr.write('{:2}: {:40} {!r} {!r}\n'.format(n, port, desc, hwid))
+        ports.append(port)
+    while True:
+        port = raw_input('Enter index ')
+        try:
+            index = int(port) - 1
+            if not 0 <= index < len(ports):
+                sys.stderr.write('Invalid index!\n')
+                continue
+        except ValueError:
+            pass
+        else:
+            port = ports[index]
+        return port    
 
 # $ python3 -m pip install pyserial
-# /dev/cu.usbserial<XYZ> for mac,
-#       ...find XYZ using $ ls /dev/cu.usb*
-# $ python3 test_ee101_1wire.py
-ser = serial.Serial("/dev/cu.usbserial-FTVHYZXQ",9600)
+# /dev/cu.usbserial-<XYZ> for mac,
+#       ...you can find your XYZ using $ python3 -m serial.tools.list_ports -v
+# My XYZ is "FTVHYZXQ", which matches my USB Serial adapter, model no. TTL232RG-VIP
+#       ...another option for finding XYZ is to use $ ls /dev/cu.usb*
+# Another useful pyserial utility is:  $ python3 -m serial.tools.miniterm
 
-# /dev/serial0 for raspberry pi 3B+
-#ser = serial.Serial("/dev/serial0",
-#                    baudrate=9600,
-#                    parity=serial.PARITY_NONE,
-#                    stopbits=serial.STOPBITS_ONE,
-#                    bytesize=serial.EIGHTBITS
-#                    )
+# Here's a simple brute force example:
+# ser = serial.Serial("/dev/cu.usbserial-FTVHYZXQ",9600)
+
+# To run:  $ python3 test_ee101_1wire.py
+
+try:
+    raw_input
+except NameError:
+    # pylint: disable=redefined-builtin,invalid-name
+    raw_input = input   # in python3 it's "raw"
+    unichr = chr  
 
 time.sleep(1)
 try:
-    #ser.write(("Hello World\r\n".encode()))
+    # ser.write(("Hello World\r\n".encode()))
+
+    user_selected_port_name = ask_for_port()
+
+    print("You selected " + user_selected_port_name)
+
+    # open serial port
+    ser = serial.Serial(user_selected_port_name,9600)
+
+    print("Press CTL+C to exit program")
 
     i = 0
 
-    #print ("Apply loopback wire from header pins 8 (uart.tx) to 10 (uart.rx)")
     while True:
         EE101Text(0,"Hello")
         EE101Text(1,"Tim")
@@ -51,8 +86,7 @@ try:
         EE101Text(7,"Python on MacOS")
 
         i += 1
-        #if i == 256:
-        #    i = 0
+
         EE101Value(0, i)
         EE101Value(1, i)
         EE101Value(2, i)
@@ -62,7 +96,7 @@ try:
         EE101Value(6, i)
         EE101Value(7, i)
 
-        # tx>rx loopback wire applied on header pins 8 and 10
+        # When tx>rx loopback is applied, one way to echo tx on rx is:
         #if ser.inWaiting() > 0:
         #    data = ser.read()
         #    print(data)
